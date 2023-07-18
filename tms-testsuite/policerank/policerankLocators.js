239df99ce236d9dce902a13f2b18532fac7f726e
hkathiria <hkathiria@egovja.com>
2023-07-18 17:20:14 +0530

    Added Police Rank feature

---
+++ tms-testsuite/policerank/policerankLocators.js
@@ -0,0 +1,12 @@
+module.exports = {
+    button: {
+        addPolicerank: "[data-test='addPoliceRank']",
+        savePolicerank: "[data-test='save']",
+        editPolicerank: "[data-test='policeRankEdit']"
+    },
+    fields: {
+        policerankCode: "[data-test='code']",
+        searchPolicerank: "[data-test='policeRankSearch']",
+        description: "[data-test='description']"
+    }
+}
