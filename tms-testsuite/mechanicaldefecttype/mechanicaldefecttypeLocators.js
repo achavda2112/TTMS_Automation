5b6fabab5f7b8902f22818e61fd38d5d3515033e
hkathiria <hkathiria@egovja.com>
2023-07-28 17:20:17 +0530

    added new feature

---
+++ tms-testsuite/mechanicaldefecttype/mechanicaldefecttypeLocators.js
@@ -0,0 +1,13 @@
+module.exports = {
+    button: {
+        addMechanicaldefecttype: "[data-test='addMechanicalDefectType']",
+        saveMechanicaldefecttype: "[data-test='save']",
+        editMechanicaldefecttype: "[data-test='mechanicalDefectTypeEdit']"
+    },
+    fields: {
+        mechanicaldefecttypeDescription: "[data-test='description']",
+        searchMechanicaldefecttype: "[data-test='mechanicalDefectTypeSearch']",
+        mechanicaldefecttypeCode: "[data-test='code']",
+        tddescription: 'td#description'
+    }
+}
